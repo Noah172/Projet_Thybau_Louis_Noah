bdd6b2ab3a7fd3df4bdf5e737705cec6f9c00239
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-14 23:07:39 +0100

    Update lecture_csv.h

--- src/modules/lecture_csv.h
+++ src/modules/lecture_csv.h
@@ -1,15 +1,9 @@
 #ifndef LECTURE_CSV_H
 #define LECTURE_CSV_H
+#include "../types.h"
 
-typedef struct {
-    int **data;
-    int rows;
-    int cols;
-} t_mat_int_dyn;
-
-
-t_mat_int_dyn lire_fichier_csv(const char *nom_fichier);
-void afficherMatrice(t_mat_int_dyn *matrice);
-
+t_mat_char_star_dyn lire_fichier_csv(const char *nom_fichier);
+void afficherMatrice(t_mat_char_star_dyn *matrice);
+void afficherMatriceInt(t_mat_int_dyn *matrice);
 
 #endif /* LECTURE_CSV_H */
