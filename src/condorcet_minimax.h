591b46d358117caceca54650b7d9d262615f4604
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-14 23:10:39 +0100

    Update condorcet_minimax.h

--- src/condorcet_minimax.h
+++ src/condorcet_minimax.h
@@ -1,10 +1,11 @@
 #ifndef CONDORCETM_H
 #define CONDORCETM_H
+#include "types.h"
 
 extern int taille_matrice;
 
-int minimax(int votes[taille_matrice][taille_matrice]);
-int condorcet_minimax(int votes[taille_matrice][taille_matrice]);
-int methodeCondorcetMinimax();
+int minimax(t_mat_int_dyn matrice);
+int condorcet_minimax(t_mat_int_dyn matrice);
+int methodeCondorcetMinimax(t_mat_int_dyn matrice, char **liste_candidat);
 
 #endif /* CONDORCET_H */
