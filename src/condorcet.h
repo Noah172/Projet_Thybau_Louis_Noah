fa652b1e4501bf1ba47f4eb431631baf9e32228e
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-14 17:31:56 +0100

    Delete src/condorcet.h
    IL FAUT PAS DE CONDORCET TT COURT

--- src/condorcet.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONDORCET_H
-#define CONDORCET_H
-
-// Déclarer les fonctions
-void condorcet(int nb_votant, int nb_candidat, int votes[nb_votant][nb_candidat], int *resultats);
-int trouverVainqueur(int *resultats);
-int methodeCondorcet();
-
-#endif
