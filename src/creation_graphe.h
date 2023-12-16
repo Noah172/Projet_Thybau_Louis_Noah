0626ed6cabdea597e32ef30dd2bd1a3df84310da
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-16 12:55:11 +0100

    Create creation_graphe.h

---
+++ src/creation_graphe.h
@@ -0,0 +1,9 @@
+#ifndef CREATION_GRAPHE_H
+#define CREATION_GRAPHE_H
+#include "types.h"
+
+t_graph creer_graphe(t_mat_int_dyn matrice, char **liste_candidat);
+
+void afficherGraphe(t_graph graphe);
+
+#endif
