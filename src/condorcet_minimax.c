3a8c9085ead0cc637da993b0f51c85d8cd09e80f
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-17 14:51:01 +0100

    Update condorcet_minimax.c

--- src/condorcet_minimax.c
+++ src/condorcet_minimax.c
@@ -1,30 +1,39 @@
 #include "condorcet_minimax.h"
+#include "list.h"
 #include "modules/lecture_csv.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int taille_matrice;
+
 int minimax(t_mat_int_dyn matrice) {
 
-  int *resultats = malloc(sizeof(int) * taille_matrice);
+  DynamicList resultats;
+  initDynamicList(&resultats);
 
   // Trouver le min de chaque duel
-  int min[2] = {1, 1};
+  DynamicList min;
+  initDynamicList(&min);
+  int index1 = 1;
+  add(&min, &index1, INT_TYPE);
+  add(&min, &index1, INT_TYPE);
+
   for (int i = 1; i < taille_matrice; i++) {
     for (int j = 0; j < taille_matrice; j++) {
-      if (matrice.data[min[0]][min[1]] > matrice.data[i][j] &&
+      if (matrice.data[get_int(&min, 0)][get_int(&min, 1)] >
+              matrice.data[i][j] &&
           matrice.data[i][j] != 0) {
-        min[0] = i;
-        min[1] = j;
+        set_int(&min, 0, i);
+        set_int(&min, 1, j);
       }
     }
-    resultats[i] = matrice.data[min[0]][min[1]];
+    set_int(&resultats, i, matrice.data[get_int(&min, 0)][get_int(&min, 1)]);
   }
 
   // trouver le max des min
   int max = 0;
   for (int i = 0; i < taille_matrice; i++) {
-    if (resultats[i] > resultats[max]) {
+    if (get_int(&resultats, i) > get_int(&resultats, max)) {
       max = i;
     }
   }
@@ -32,11 +41,11 @@ int minimax(t_mat_int_dyn matrice) {
   // verifier s'il y a des égalité
   int egalite = 0;
   for (int i = 0; i < taille_matrice; i++) {
-    if (resultats[i] == resultats[max] && i != max)
+    if (get_int(&resultats, i) == get_int(&resultats, max) && i != max)
       egalite = 1;
   }
 
-  free(resultats);
+  freeDynamicList(&resultats);
 
   if (egalite)
     return -1;
@@ -45,13 +54,19 @@ int minimax(t_mat_int_dyn matrice) {
 }
 
 int condorcet_minimax(t_mat_int_dyn matrice) {
-  // chercher le max de chaque ligne
-  int max[2] = {1, 0};
+
+  DynamicList max;
+  initDynamicList(&max);
+  int index0 = 1;
+  int index1 = 0;
+  add(&max, &index0, INT_TYPE);
+  add(&max, &index1, INT_TYPE);
   for (int i = 1; i < taille_matrice; i++) {
     for (int j = 0; j < taille_matrice; j++) {
-      if (matrice.data[max[0]][max[1]] < matrice.data[i][j]) {
-        max[0] = i;
-        max[1] = j;
+      if (matrice.data[get_int(&max, 0)][get_int(&max, 1)] <
+          matrice.data[i][j]) {
+        set_int(&max, 0, i);
+        set_int(&max, 1, j);
       }
     }
   }
@@ -60,7 +75,9 @@ int condorcet_minimax(t_mat_int_dyn matrice) {
   int c = 0;
   for (int i = 1; i < taille_matrice; i++) {
     for (int j = 0; j < taille_matrice; j++) {
-      if (matrice.data[max[0]][max[1]] == matrice.data[i][j] && max[0] != i) {
+      if (matrice.data[get_int(&max, 0)][get_int(&max, 1)] ==
+              matrice.data[i][j] &&
+          get_int(&max, 0) != i) {
         c++;
       }
     }
@@ -68,14 +85,14 @@ int condorcet_minimax(t_mat_int_dyn matrice) {
 
   // si c vaut 0, alors il n'y a pas d'égalité et on a un vainqueur
   if (c == 0) {
-    return max[0];
+    return get_int(&max, 0);
   } else {
     printf("Il n'y a pas de vainqueur, tentons la méthode de minimax\n");
     return minimax(matrice);
   }
 }
 
-int methodeCondorcetMinimax(t_mat_int_dyn matrice, char **liste_candidat) {
+int methodeCondorcetMinimax(t_mat_int_dyn matrice, DynamicList liste_candidat) {
   printf("\n\n\033[1;34m======================================================="
          "=====\n");
   printf("============================================================\n");
@@ -93,6 +110,6 @@ int methodeCondorcetMinimax(t_mat_int_dyn matrice, char **liste_candidat) {
     printf("Il n'y a pas de vainqueur par la méthode Condorcet et Minimax\n");
   else
     printf("Le vainqueur par la méthode de condorcet est est le candidat %s\n",
-           liste_candidat[vainqueur]);
+           get_char(&liste_candidat, vainqueur));
   return 0;
 }
