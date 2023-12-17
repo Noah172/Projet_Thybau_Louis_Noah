f132b3146b96c0c9d9cbca065a1915feb1dc6905
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-17 14:21:31 +0100

    Update list.h

--- src/list.h
+++ src/list.h
@@ -2,9 +2,19 @@
 #define LIST_H
 #include "types.h"
 
+//-------------------------------------------------------------------------------
+//
+//                   LIST CLASSIQUE
+//
+//-------------------------------------------------------------------------------
+
 void initDynamicList(DynamicList *list);
 
-void add(DynamicList *list, int data);
+void add(DynamicList *list, void *data, DataType type);
+
+int get_int(DynamicList *list, int index);
+
+char *get_char(DynamicList *list, int index);
 
 void removeByIndex(DynamicList *list, int index);
 
@@ -12,13 +22,21 @@ void printDynamicList(DynamicList *list);
 
 void freeDynamicList(DynamicList *list);
 
-
+//-------------------------------------------------------------------------------
+//
+//                   LIST CIRCULAIRE
+//
+//-------------------------------------------------------------------------------
 
 void initList(CircularList *list);
 
-void addHead(CircularList *list, int data);
+void addHead(CircularList *list, void *data, DataType type);
 
-void addTail(CircularList *list, int data);
+void addTail(CircularList *list, void *data, DataType type);
+
+int gett_int(CircularList *list, int index);
+
+char *gett_char(CircularList *list, int index);
 
 void removeHead(CircularList *list);
 
