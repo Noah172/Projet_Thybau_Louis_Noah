9ab14bc03bc20ec8df9bf1eb97f5215b1209890e
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-17 03:22:01 +0100

    Update types.h

--- src/types.h
+++ src/types.h
@@ -1,5 +1,26 @@
 #ifndef TYPES_H
 #define TYPES_H
+
+// STRUCTURE DE DONNEE
+
+// Structure d'un élément de la liste
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+// Structure de la liste
+typedef struct {
+  Node *head;
+  Node *tail;
+  int nb_elem;
+} CircularList;
+
+typedef struct {
+  Node *head;
+  int size;
+} DynamicList;
+
 typedef struct {
   int **data;
   int rows;
@@ -20,10 +41,10 @@ typedef struct {
   int score;
 } t_resultat_uninominale;
 
-typedef struct{
-    int id;
-    int *vote;
-}t_votants;
+typedef struct {
+  int id;
+  int *vote;
+} t_votants;
 
 typedef struct {
   t_votants **votants;
@@ -32,12 +53,19 @@ typedef struct {
   char **candidats;
 } t_votes;
 
+// GRAPHE
 
-/*typedef struct t_sommet {
+typedef struct t_sommet {
   char *valeur;
   struct t_sommet *suivant; // Sommet suivant dans le graphe
   struct t_sommet *origine; // Sommet d'origine dans le graphe
-} t_sommet;*/
+} t_sommet;
+
+/*typedef struct {
+  t_sommet *source;
+  t_sommet *destination;
+  int poids;
+} t_arc;*/
 
 typedef struct {
   char *source;
@@ -50,5 +78,11 @@ typedef struct {
   t_sommet **sommets;
   int count;
 } t_graph;
+
+// Structure pour représenter un cycle
+typedef struct {
+  t_arc *arcs;
+  int count;
+} t_cycle;
 
 #endif // TYPES_H
