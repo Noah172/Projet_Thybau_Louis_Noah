710adcd43ce8c37357ee1ef6dcab221bd07c9a59
Noah172 <114427265+Noah172@users.noreply.github.com>
2023-12-17 20:45:46 +0100

    Update jugement_majoritaire.h

--- src/jugement_majoritaire.h
+++ src/jugement_majoritaire.h
@@ -1,22 +1,11 @@
-// #ifndef JUGEMENT_MAJORITAIRE_H
-// #define JUGEMENT_MAJORITAIRE_H
-//
-// #include "modules/csv_votes.h"
-//
-// // Structure pour stocker les résultats du jugement majoritaire
-// typedef struct {
-//     int *points;  // Tableau des points de chaque candidat
-// } t_resultat_majoritaire;
-//
-// // Fonction pour initialiser la structure des résultats du jugement majoritaire
-// t_resultat_majoritaire initialiser_resultat_majoritaire(int nb_candidats);
-//
-// // Fonction pour effectuer le jugement majoritaire
-// t_resultat_majoritaire jugement_majoritaire(t_vote_format2_data data);
-//
-// // Fonction pour afficher les résultats du jugement majoritaire
-// void afficher_resultat_majoritaire(t_resultat_majoritaire resultat, t_vote_format2_data data);
-//
-// int jugementMajoritaire() ;
-//
-// #endif  // JUGEMENT_MAJORITAIRE_H
+#ifndef JUGEMENT_MAJORITAIRE_H
+#define JUGEMENT_MAJORITAIRE_H
+
+#include "types.h"
+
+
+
+void jugementMajoritaire(t_mat_char_star_dyn votes);
+
+#endif // JUGEMENT_MAJORITAIRE_H
+
