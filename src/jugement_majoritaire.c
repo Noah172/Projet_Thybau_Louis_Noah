a01f7334926f13348e566b9a4f3133ee7388df21
Noah172 <114427265+Noah172@users.noreply.github.com>
2023-12-17 20:44:59 +0100

    Update jugement_majoritaire.c

--- src/jugement_majoritaire.c
+++ src/jugement_majoritaire.c
@@ -1,88 +1,65 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include "modules/csv_votes.h"
-//
-// #define NB_MENTIONS 6  // Mention de 0 à 5
-//
-// // Structure pour stocker les résultats du jugement majoritaire
-// typedef struct {
-//     int *points;  // Tableau des points de chaque candidat
-// } t_resultat_majoritaire;
-//
-// // Fonction pour initialiser la structure des résultats du jugement majoritaire
-// t_resultat_majoritaire initialiser_resultat_majoritaire(int nb_candidats) {
-//     t_resultat_majoritaire resultat;
-//     resultat.points = calloc(nb_candidats, sizeof(int));
-//     return resultat;
-// }
-//
-// // Fonction pour effectuer le jugement majoritaire
-// t_resultat_majoritaire jugement_majoritaire(t_vote_format2_data data) {
-//     int nb_candidats = data.nb_candidats;
-//     int nb_votes = data.nb_votes;
-//
-//     // Initialiser la structure des résultats
-//     t_resultat_majoritaire resultat = initialiser_resultat_majoritaire(nb_candidats);
-//
-//     // Parcourir les votes
-//     for (int i = 0; i < nb_votes; ++i) {
-//         t_vote_format2 vote = data.votes[i];
-//
-//         // Attribution des points selon les mentions
-//         for (int j = 0; j < nb_candidats; ++j) {
-//             int mention = vote.votes[j];
-//             resultat.points[j] += NB_MENTIONS - mention;
-//         }
-//     }
-//
-//     return resultat;
-// }
-//
-// // Fonction pour afficher les résultats du jugement majoritaire
-// void afficher_resultat_majoritaire(t_resultat_majoritaire resultat, t_vote_format2_data data) {
-//     int nb_candidats = data.nb_candidats;
-//
-//     printf("Résultat du jugement majoritaire :\n");
-//
-//     for (int i = 0; i < nb_candidats; ++i) {
-//         printf("Candidat %s : %d points\n", data.noms_candidats[i], resultat.points[i]);
-//     }
-//
-//     // Trouver le candidat avec le plus de points
-//     int max_points = resultat.points[0];
-//     int index_max = 0;
-//
-//     for (int i = 1; i < nb_candidats; ++i) {
-//         if (resultat.points[i] > max_points) {
-//             max_points = resultat.points[i];
-//             index_max = i;
-//         }
-//     }
-//
-//     printf("Le gagnant est : %s\n", data.noms_candidats[index_max]);
-// }
-//
-// int jugementMajoritaire() {
-//     // Remplacez "votre_fichier.csv" par le nom de votre fichier CSV
-//     t_vote_format2_data data = lire_fichier_csv_format2("fich_tests/vote10");
-//
-//     // Effectuer le jugement majoritaire
-//     t_resultat_majoritaire resultat = jugement_majoritaire(data);
-//
-//     // Afficher les résultats
-//     afficher_resultat_majoritaire(resultat, data);
-//
-//     // Libérer la mémoire
-//     free(resultat.points);
-//     for (int i = 0; i < data.nb_candidats; ++i) {
-//         free(data.noms_candidats[i]);
-//     }
-//     free(data.noms_candidats);
-//     for (int i = 0; i < data.nb_votes; ++i) {
-//         free(data.votes[i].votes);
-//     }
-//     free(data.votes);
-//
-//     return 0;
-// }
+#include "jugement_majoritaire.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+void jugementMajoritaire(t_mat_char_star_dyn votes) {
+    t_resultat_majoritaire resultat;
+    int nb_candidats = votes.cols - 5; // Il semble que les colonnes 0 à 3 ne sont pas utilisées
+    int nb_votants = votes.rows - 1;
+
+    resultat.nb_candidats = nb_candidats;
+    resultat.nb_votants = nb_votants;
+    resultat.candidats = malloc(nb_candidats * sizeof(char *));
+    resultat.notes = malloc(nb_candidats * sizeof(int));
+    resultat.medianes = malloc(nb_candidats * sizeof(int));
+
+    if (resultat.candidats == NULL || resultat.notes == NULL || resultat.medianes == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < nb_candidats; i++) {
+        resultat.candidats[i] = votes.data[0][i + 4];
+        resultat.notes[i] = 0;
+    }
+
+    for (int i = 0; i < nb_candidats; i++) {
+        int *valeurs = malloc(nb_votants * sizeof(int));
+        if (valeurs == NULL) {
+            exit(EXIT_FAILURE);
+        }
+
+        for (int j = 0; j < nb_votants; j++) {
+            valeurs[j] = atoi(votes.data[j + 1][i + 4]);
+        }
+
+        qsort(valeurs, nb_votants, sizeof(int), compare);
+        if (nb_votants % 2 == 1) {
+            resultat.medianes[i] = valeurs[nb_votants / 2];
+        } else {
+            resultat.medianes[i] = (valeurs[nb_votants / 2 - 1] + valeurs[nb_votants / 2]) / 2;
+        }
+
+        free(valeurs);
+    }
+
+    int index_gagnant = 0;  // Supposons que le premier candidat a la plus petite médiane initialement
+
+    for (int i = 1; i < nb_candidats; i++) {
+        if (resultat.medianes[i] < resultat.medianes[index_gagnant]) {
+            index_gagnant = i;  // Met à jour l'index du candidat avec la plus petite médiane
+        }
+    }
+
+    printf("Vainqueur : %s\n", resultat.candidats[index_gagnant]);
+
+
+    free(resultat.candidats);
+    free(resultat.notes);
+    free(resultat.medianes);
+}
