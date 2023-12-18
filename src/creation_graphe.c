8aa7fdb60363838735f91544276c9efbc6b3d176
thybaupr <71831637+SalutCMoiThybau@users.noreply.github.com>
2023-12-18 23:34:23 +0100

    (Thybau) doc creation_graphe.c

--- src/creation_graphe.c
+++ src/creation_graphe.c
@@ -1,44 +1,18 @@
-/// \file creation_graphe.c
-/// \brief Créer le graphe à partir de la matrice de duet et le confirmer
-/// \brief ATTENTION : PAS TERMINÉ
-
+/* \file creation_graphe.c
+ * \brief Création et manipulation d'un graphe à partir d'une matrice de duels
+ */
 #include "list.h"
 #include "types.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/// \fn compare_arcs(const void *a, const void *b)
-/// \brief Fonction de comparaison pour le tri rapide des arcs du graphe.
-/// \param a Pointeur vers le premier arc.
-/// \param b Pointeur vers le deuxième arc.
-/// \return Valeur négative si le poids du premier arc est inférieur à celui du
-/// deuxième, 0 en cas d'égalité, et positive sinon.
-/*int compare_arcs(const void *a, const void *b) {
-  return ((t_arc *)b)->poids - ((t_arc *)a)->poids;
-}*/
 
-/// \fn void trier_graphe(t_graph *graphe)
-/// \brief Fonction pour trier les arcs du graphe dans l'ordre décroissant par
-/// poids. \param graphe Pointeur vers la structure du graphe.
-/*void trier_graphe(t_graph *graphe) {
-  qsort(graphe->arcs, graphe->count, sizeof(t_arc), compare_arcs);
-}*/
-
-/// \fn void confirmerGraphe(t_graph *graphe)
-/// \brief Fonction pour confirmer le graphe en vérifiant la présence de cycles.
-/// \param graphe Pointeur vers la structure du graphe.
-/*void confirmerGraphe(t_graph *graphe) {
-  for (int i = 0; i < graphe->count; i++) {
-    int *visited = malloc(graphe->count * sizeof(int));
-    int *recStack = malloc(graphe->count * sizeof(int));
-    memset(visited, 0, graphe->count * sizeof(int));
-    memset(recStack, 0, graphe->count * sizeof(int));
-
-    /// \brief A FINIR
-  }
-}*/
-
+/** \fn Sommet* createNode(char* data)
+ *  \brief Crée un nouveau sommet avec la donnée spécifiée.
+ *  \param data Donnée associée au sommet.
+ *  \return Pointeur vers le nouveau sommet.
+ */
 Sommet* createNode(char* data) {
   Sommet* newSommet= (Sommet*)malloc(sizeof(Sommet));
     newSommet->data = data;
@@ -48,6 +22,10 @@ Sommet* createNode(char* data) {
     return newSommet;
 }
 
+/** \fn Graph* createGraph()
+ *  \brief Crée un nouveau graphe vide.
+ *  \return Pointeur vers le nouveau graphe.
+ */
 Graph* createGraph() {
     Graph* graph = (Graph*)malloc(sizeof(Graph));
     graph->numNodes = 0;
@@ -55,6 +33,11 @@ Graph* createGraph() {
     return graph;
 }
 
+/** \fn void addNode(Graph* graph, char* data)
+ *  \brief Ajoute un nouveau sommet au graphe avec la donnée spécifiée.
+ *  \param graph Pointeur vers le graphe.
+ *  \param data Donnée associée au sommet.
+ */
 void addNode(Graph* graph, char* data) {
   Sommet* newSommet= createNode(data);
 
@@ -64,6 +47,12 @@ void addNode(Graph* graph, char* data) {
     graph->numNodes++;
 }
 
+/** \fn void addEdge(Sommet* src, Sommet* dest, int poids)
+ *  \brief Ajoute une arête pondérée entre deux sommets.
+ *  \param src Pointeur vers le sommet source.
+ *  \param dest Pointeur vers le sommet de destination.
+ *  \param poids Poids de l'arête.
+ */
 void addEdge( Sommet* src, Sommet* dest,int poids) {
     src->next = (Sommet**)realloc(src->next, (src->nb_next + 1) * sizeof(Sommet*));
     src->next_value = (int*)realloc(src->next_value, (src->nb_next + 1) * sizeof(int));
@@ -74,6 +63,10 @@ void addEdge( Sommet* src, Sommet* dest,int poids) {
     src->nb_next++;
 }
 
+/** \fn void printGraph(Graph* graph)
+ *  \brief Affiche le graphe avec les données des sommets et les poids des arêtes.
+ *  \param graph Pointeur vers le graphe à afficher.
+ */
 void printGraph(Graph* graph) {
     for (int i = 0; i < graph->numNodes; ++i) {
         Sommet* currentSommet = graph->sommets[i];
