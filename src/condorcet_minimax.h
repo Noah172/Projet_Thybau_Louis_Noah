1649ac68ac9c4802a5ce3353e0671b0be174ef31
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-18 01:32:00 +0100

    Update condorcet_minimax.h

--- src/condorcet_minimax.h
+++ src/condorcet_minimax.h
@@ -1,11 +1,11 @@
 #ifndef CONDORCETM_H
 #define CONDORCETM_H
+#include <stdio.h>
 #include "types.h"
 
 extern int taille_matrice;
 
-int minimax(t_mat_int_dyn matrice);
-int condorcet_minimax(t_mat_int_dyn matrice);
-int methodeCondorcetMinimax(t_mat_int_dyn matrice, DynamicList liste_candidat);
+char *methodeCondorcetMinimax(t_mat_int_dyn matrice,
+                              DynamicList liste_candidat, FILE* fichier);
 
 #endif /* CONDORCET_H */
