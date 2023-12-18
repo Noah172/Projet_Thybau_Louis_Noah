9b2fede53e2112cbc1e3f69a62f976c4f19e4918
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-18 01:31:24 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,6 +11,49 @@
 #include <string.h>
 
 t_mat_int_dyn
+creation_matrice_int_depuis_vote_ballots(t_mat_char_star_dyn *mat_votes) {
+  DynamicList liste_candidat;
+  initDynamicList(&liste_candidat);
+
+  for (int i = 4; i < mat_votes->cols; i++) {
+    add(&liste_candidat, mat_votes->data[0][i], CHAR_TYPE);
+  }
+
+  // creer matrce de duel
+  t_mat_int_dyn matrice_duel;
+  matrice_duel.cols = mat_votes->cols - 4;
+  matrice_duel.rows = mat_votes->cols - 4;
+  matrice_duel.data = malloc(matrice_duel.rows * sizeof(int *));
+
+  // Allouer de la mémoire pour chaque ligne de la nouvelle matrice
+  for (int i = 0; i < matrice_duel.rows; ++i) {
+    matrice_duel.data[i] = malloc(matrice_duel.cols * sizeof(int));
+  }
+
+  int nb_candidats = mat_votes->cols - 4;
+  int nb_votants = mat_votes->rows;
+  for (int i = 0; i < nb_candidats; i++) {
+    matrice_duel.data[i] = (int *)malloc(nb_candidats * sizeof(int));
+    memset(matrice_duel.data[i], 0, nb_candidats * sizeof(int));
+  }
+  for (int i = 1; i < nb_votants; i++) {
+    for (int j = 0; j < nb_candidats; j++) {
+      for (int k = j; k < nb_candidats; k++) {
+        int classement_candidat1 = atoi(mat_votes->data[i][j + 4]);
+        int classement_candidat2 = atoi(mat_votes->data[i][k + 4]);
+
+        if (classement_candidat1 < classement_candidat2) {
+          matrice_duel.data[j][k]++;
+        } else if (classement_candidat1 > classement_candidat2) {
+          matrice_duel.data[k][j]++;
+        }
+      }
+    }
+  }
+  return matrice_duel;
+}
+
+t_mat_int_dyn
 creation_matrice_int_depuis_char(t_mat_char_star_dyn *matrice_char) {
   t_mat_int_dyn matrice_duel;
   matrice_duel.cols = matrice_char->cols;
@@ -89,7 +132,6 @@ void lire_balise(int argc, char *argv[]) {
   }
 }
 
-
 int main(int argc, char *argv[]) {
 
   if (argc < 3) {
@@ -106,41 +148,120 @@ int main(int argc, char *argv[]) {
 
   lire_balise(argc, argv);
 
+  t_mat_char_star_dyn mat_votes = lire_fichier_csv(nom_fichier);
+  t_mat_int_dyn matrice_duels;
+  DynamicList liste_candidat;
+  initDynamicList(&liste_candidat);
+
+  for (int i = 0; i < mat_votes.cols; i++) {
+    add(&liste_candidat, mat_votes.data[0][i], CHAR_TYPE);
+  }
   if (i) {
-    t_votes *mat_votes = lireVotesDepuisCSV(nom_fichier);
-    afficherVotes(mat_votes);
-    // creation des matrices char et int depuis le t_votes
+
+    if (strcmp(mat_votes.data[0][0], "ID votant") == 0) {
+      matrice_duels = creation_matrice_int_depuis_vote_ballots(&mat_votes);
+    } else {
+      matrice_duels = creation_matrice_int_depuis_char(&mat_votes);
+    }
   }
 
   if (d) {
-    t_mat_char_star_dyn mat_votes;
-    mat_votes = lire_fichier_csv(nom_fichier);
-    afficherMatrice(&mat_votes);
-    DynamicList liste_candidat;
-    initDynamicList(&liste_candidat);
 
-    for(int i = 0; i < mat_votes.cols; i++){
-      add(&liste_candidat, mat_votes.data[0][i], CHAR_TYPE);
-    }
-    
-    //char **liste_candidat = mat_votes.data[0];
-    
-    t_mat_int_dyn mat_duel = creation_matrice_int_depuis_char(&mat_votes);
+    matrice_duels = creation_matrice_int_depuis_char(&mat_votes);
 
-    methodeCondorcetMinimax(mat_duel, liste_candidat);
-    
     /*t_graph graphe = creer_graphe(mat_duel, liste_candidat);
 
     confirmerGraphe(&graphe);
 
     afficherGraphe(graphe);*/
   }
+  FILE *fichier = stdout;
   
+  if (param_o != NULL) {
+    fichier = fopen(param_o,"w"); // Ouverture du fichier en mode écriture ("w")
 
-  return 0;
+    if (fichier == NULL) {
+      fichier = stdout;
+    }
+  }
+  
+  char *vainqueur_condor = NULL;
+  char *vainqueur_uni1 = NULL;
+  ;
+  char *vainqueur_uni2 = NULL;
+  ;
+  char *vainqueur_jm = NULL;
+  ;
 
-  // methodeUninominales();
-  // jugementMajoritaire();
+  if (m) {
+    if (strcmp(param_m, "uni1") == 0) {
+      // vainqueur_uni1 = methodeUninominales(..., fichier);
+    } else if (strcmp(param_m, "uni2") == 0) {
+      // vainqueur_uni2 = methodeUninominales(..., fichier);
+    } else if (strcmp(param_m, "cm") == 0) {
+      vainqueur_condor = methodeCondorcetMinimax(matrice_duels, liste_candidat, fichier);
+    } else if (strcmp(param_m, "cp") == 0) {
+      // PAS IMPLÉMENTÉ :/
+    } else if (strcmp(param_m, "cs") == 0) {
+      // PAS IMPLÉMENTÉ :/
+    } else if (strcmp(param_m, "jm") == 0) {
+      // vainqueur_jm = jugementMajoritaire(..., fichier);
+    } else if (strcmp(param_m, "all") == 0) {
+      // vainqueur_uni1 = methodeUninominales(..., fichier);
+      // vainqueur_uni2 = methodeUninominales(..., fichier);
+      vainqueur_condor = methodeCondorcetMinimax(matrice_duels, liste_candidat, fichier);
+      // vainqueur_jm = jugementMajoritaire(..., fichier);
+    }
+  }
+
+  if (vainqueur_condor != NULL) {
+    printf("\n\n\033[1;34m====================================================="
+           "======\n");
+    printf("=====================\033[1;31mCONDORCET "
+           "MINIMAX\033[1;34m======================\n");
+    printf("============================================================"
+           "\n\n\033[0m");
+
+    if (strcmp(vainqueur_condor, "NULL") == 0) {
+      printf("Il n'y a pas de vainqueur par la méthode de Condorcet Minimax\n");
+    } else {
+      printf("Le vainqueur par la méthode de Condorcet Minimax est %s\n",
+             vainqueur_condor);
+    }
+  }
+
+  if (vainqueur_uni1 != NULL) {
+    printf("\n\n\033[1;34m====================================================="
+           "======\n");
+    printf("=====================\033[1;31mUNINOMINAL 1 "
+           "TOUR\033[1;34m======================\n");
+    printf("============================================================"
+           "\n\n\033[0m");
+
+    /*TRAITEMENT AFFICHAGE*/
+  }
+
+  if (vainqueur_uni2 != NULL) {
+    printf("\n\n\033[1;34m====================================================="
+           "======\n");
+    printf("=====================\033[1;31mUNINOMINAL 2 "
+           "TOURS\033[1;34m======================\n");
+    printf("============================================================"
+           "\n\n\033[0m");
+
+    /*TRAITEMENT AFFICHAGE*/
+  }
+
+  if (vainqueur_jm != NULL) {
+    printf("\n\n\033[1;34m====================================================="
+           "======\n");
+    printf("=====================\033[1;31mJUGEMENT "
+           "MAJORITAIRE\033[1;34m======================\n");
+    printf("============================================================"
+           "\n\n\033[0m");
+
+    /*TRAITEMENT AFFICHAGE*/
+  }
 
   free(nom_fichier);
   return 0;
