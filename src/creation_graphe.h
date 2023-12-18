0241fcc2bc64f3a1917d72b7f86573bfd33968f3
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-18 22:11:56 +0100

    Update creation_graphe.h

--- src/creation_graphe.h
+++ src/creation_graphe.h
@@ -2,10 +2,12 @@
 #define CREATION_GRAPHE_H
 #include "types.h"
 
-t_graph creer_graphe(t_mat_int_dyn matrice, char **liste_candidat);
+Graph* createGraph();
 
-void afficherGraphe(t_graph graphe);
+void addNode(Graph* graph, int data);
 
-void confirmerGraphe(t_graph *graphe);
+void addEdge(Graph* graph, int src, int dest);
+
+void printGraph(Graph* graph);
 
 #endif
