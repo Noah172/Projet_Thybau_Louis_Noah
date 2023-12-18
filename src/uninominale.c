5f4162af35f107dacbd88dfb840833fefe20e2f9
thybaupr <71831637+SalutCMoiThybau@users.noreply.github.com>
2023-12-18 01:38:26 +0100

    Update uninominale.c

--- src/uninominale.c
+++ src/uninominale.c
@@ -1,191 +1,220 @@
-// #define _DEFAULT_SOURCE
-// #define _SVID_SOURCE
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include "uninominale.h"
-// #include "modules/lecture_csv.h"
-// #include "modules/csv_votes.h"
-//
-//
-//
-// t_mat_int_dyn init_mat_int_dyn(int rows, int cols) {
-//   t_mat_int_dyn mat;
-//   mat.data = (int **)malloc(rows * sizeof(int *));
-//   for (int i = 0; i < rows; ++i) {
-//     mat.data[i] = (int *)malloc(cols * sizeof(int));
-//   }
-//   mat.rows = rows;
-//   mat.cols = cols;
-//   return mat;
+#define _DEFAULT_SOURCE
+#define _SVID_SOURCE
+#include "uninominale.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+/**
+ * \brief Trouve le vainqueur avec la méthode uninominale.
+ *
+ * Cette fonction identifie le candidat vainqueur en utilisant la méthode uninominale,
+ * où la note la plus basse est considérée comme la meilleure.
+ *
+ * \param[in] mat_votes La matrice des votes, représentant les évaluations de chaque votant pour chaque candidat.
+ * \return Le nom du candidat vainqueur.
+ *
+ * \note La fonction suppose que les votes sont stockés dans la matrice, avec les noms de candidats
+ * commençant à la colonne 4 et les votes correspondants commençant à la ligne 1.
+ *
+ * \remarks La fonction alloue de la mémoire pour stocker les scores intermédiaires et le nom du vainqueur,
+ * assurez-vous de libérer correctement cette mémoire après utilisation.
+ */
+char* trouver_vainqueur(t_mat_char_star_dyn *mat_votes) {
+	int nb_candidats = mat_votes->cols - 4;
+
+	// initialiser le tableau des scores
+	int *scores = (int *)malloc(nb_candidats * sizeof(int));
+	for (int i = 0; i < nb_candidats; ++i) {
+		scores[i] = 0;
+	}
+
+	// parcourir les votes et maj les scores
+	for (int row = 1; row < mat_votes->rows; ++row) {
+		for (int col = 4; col < mat_votes->cols; ++col) {
+			int vote = atoi(mat_votes->data[row][col]);
+			if (vote > 0) {
+				scores[col - 4] += vote;
+			}
+		}
+	}
+
+	// trouver le candidat avec le score le plus bas
+	int min_score = scores[0];
+	int index_winner = 0;
+
+	for (int i = 1; i < nb_candidats; ++i) {
+		if (scores[i] < min_score) {
+			min_score = scores[i];
+			index_winner = i;
+		}
+	}
+
+	// récupérer le nom du vainqueur
+	char *vainqueur = strdup(mat_votes->data[0][index_winner + 4]);
+
+	// libérer la mémoire allouée pour les scores
+	free(scores);
+
+	return vainqueur;
+}
+
+/**
+ * \brief Calcule le score du candidat gagnant avec la méthode uninominale.
+ *
+ * Cette fonction calcule le score du candidat gagnant en pourcentage, en utilisant la méthode uninominale.
+ * Le score est déterminé en considérant la note la plus basse comme la meilleure, et en comptant le nombre de votants
+ * ayant donné leur vote au candidat gagnant.
+ *
+ * \param[in] mat_votes La matrice des votes, représentant les évaluations de chaque votant pour chaque candidat.
+ * \param[in] vainqueur Le nom du candidat gagnant.
+ * \return Le score en pourcentage du candidat gagnant.
+ *
+ * \note La fonction suppose que les votes sont stockés dans la matrice, avec les noms de candidats
+ * commençant à la colonne 4 et les votes correspondants commençant à la ligne 1.
+ *
+ * \remarks La fonction utilise l'algorithme pour trouver le vote le plus bas, et compte les votants qui ont voté
+ * pour le candidat gagnant.
+ *
+ * \warning Assurez-vous que le candidat gagnant passé en paramètre correspond à un candidat présent dans la matrice,
+ * sinon les résultats peuvent être imprévisibles.
+ */
+int calculer_score(t_mat_char_star_dyn *mat_votes, char *vainqueur) {
+	int nb_votants = mat_votes->rows - 1;
+
+	// trouver l'index du candidat gagnant
+	int index_winner = -1;
+	for (int i = 4; i < mat_votes->cols; ++i) {
+		if (strcmp(mat_votes->data[0][i], vainqueur) == 0) {
+			index_winner = i;
+			break;
+		}
+	}
+	
+	// calculer le score en pourcentage
+	int total_winner_votes = 0;
+
+	for (int row = 1; row < mat_votes->rows; ++row) {
+		int min_vote = atoi(mat_votes->data[row][4]);
+		int index = 4;
+		for (int col = 5; col < mat_votes->cols; ++col) {
+			if (atoi(mat_votes->data[row][col]) < min_vote || (atoi(mat_votes->data[row][col]) == min_vote && col == index_winner)) {
+				min_vote = atoi(mat_votes->data[row][col]);
+				index = col;
+			}
+		}
+		if (index == index_winner) {
+			total_winner_votes += 1;
+		}
+	}
+	int score = (total_winner_votes * 100) / nb_votants;
+
+	return score;
+}
+
+/**
+ * \brief Effectue un scrutin uninominal à un tour et renvoie les résultats.
+ *
+ * Cette fonction réalise un scrutin uninominal à un tour en utilisant la méthode uninominale,
+ * trouve le vainqueur et calcule son score en pourcentage. Les résultats sont stockés dans une structure
+ * de données t_resultat_uninominale.
+ *
+ * \param[in] mat_votes La matrice des votes, représentant les évaluations de chaque votant pour chaque candidat.
+ * \return Une structure t_resultat_uninominale contenant le nombre de candidats, le nombre de votants, le vainqueur
+ * et son score en pourcentage.
+ *
+ * \note La fonction suppose que les votes sont stockés dans la matrice, avec les noms de candidats
+ * commençant à la colonne 4 et les votes correspondants commençant à la ligne 1.
+ *
+ * \warning Assurez-vous que la matrice de votes est correctement formatée pour représenter un scrutin uninominal à un tour.
+ */
+t_resultat_uninominale uninominale_un_tour(t_mat_char_star_dyn *mat_votes) {
+	t_resultat_uninominale resultat;
+
+	// trouver le vainqueur
+	char *vainqueur = trouver_vainqueur(mat_votes);
+
+	// remplir la structure de résultat
+	resultat.nb_candidats = mat_votes->cols - 4;
+	resultat.nb_votants = mat_votes->rows - 1;
+	resultat.vainqueur = strdup(vainqueur);
+	resultat.score = calculer_score(mat_votes, vainqueur); // calculer le score
+
+	return resultat;
+}
+
+// Fonction pour le scrutin uninominal à deux tours
+t_resultat_uninominale uninominale_deux_tours(t_mat_char_star_dyn *mat_votes) {
+	t_resultat_uninominale resultat1 = uninominale_un_tour(mat_votes);
+	return resultat1;
+	/*******************************************
+	t_resultat_uninominale resultat;
+	int nb_votants = mat_votes->rows - 1;
+
+	// Premier tour
+	t_resultat_uninominale premier_tour_resultat = uninominale_un_tour(mat_votes);
+	char *premier_tour_vainqueur = strdup(premier_tour_resultat.vainqueur);
+
+	// Deuxième tour virtuel
+	char *second_tour_candidat1 = strdup(premier_tour_vainqueur);
+	char *second_tour_candidat2 = trouver_deuxieme_tour_candidat(mat_votes, second_tour_candidat1);
+
+	// Créer une matrice de votes pour le deuxième tour
+	t_mat_char_star_dyn mat_votes_tour2;
+	init_mat_char_star_dyn(&mat_votes_tour2, mat_votes->rows, mat_votes->cols);
+
+	for (int row = 0; row < mat_votes->rows; ++row) {
+		mat_votes_tour2.data[row][0] = strdup(mat_votes->data[row][0]);  // Copie ID votant
+		mat_votes_tour2.data[row][1] = strdup(mat_votes->data[row][1]);  // Copie Date
+		mat_votes_tour2.data[row][2] = strdup(mat_votes->data[row][2]);  // Copie Code vote
+		mat_votes_tour2.data[row][3] = strdup(mat_votes->data[row][3]);  // Copie Votant
+		mat_votes_tour2.data[row][4] = strdup(mat_votes->data[row][getIndexFromName(mat_votes, second_tour_candidat1)]);
+		mat_votes_tour2.data[row][5] = strdup(mat_votes->data[row][getIndexFromName(mat_votes, second_tour_candidat2)]);
+	}
+
+	// Deuxième tour réel
+	t_resultat_uninominale deuxieme_tour_resultat = uninominale_un_tour(&mat_votes_tour2);
+
+	// Remplir la structure de résultat
+	resultat.nb_candidats = mat_votes->cols - 4;
+	resultat.nb_votants = nb_votants;
+	resultat.vainqueur = strdup(deuxieme_tour_resultat.vainqueur);
+	resultat.score = deuxieme_tour_resultat.score;
+
+	// Libérer la mémoire allouée
+	free_mat_char_star_dyn(&mat_votes_tour2);
+	free(premier_tour_vainqueur);
+	free(second_tour_candidat1);
+	free(second_tour_candidat2);
+
+	return resultat;
+	****************************************************/
+}
+
+
+
+// fonction pour afficher les résultats d'un scrutin uninominal
+// void afficher_resultat_uninominal(t_resultat_uninominale resultat, bool deuxTours) {
+// 	printf("Mode de scrutin : uninominal %s, %d candidats, %d votants, vainqueur = %s, score = %d%%\n",
+// 		   (deuxTours) ? "à deux tours" : "à un tour", resultat.nb_candidats, resultat.nb_votants,
+// 		   resultat.vainqueur, resultat.score);
 // }
-//
-// void free_mat_int_dyn(t_mat_int_dyn *mat) {
-//   for (int i = 0; i < mat->rows; ++i) {
-//     free(mat->data[i]);
-//   }
-//   free(mat->data);
-// }
-//
-// t_resultat_uninominale init_resultat_uninominale(int nb_candidats) {
-//   t_resultat_uninominale resultat;
-//   resultat.candidats = (char **)malloc(nb_candidats * sizeof(char *));
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     // Allouer de la mémoire pour le nom du candidat
-//     resultat.candidats[i] = (char *)malloc(100 * sizeof(char));
-//     strcpy(resultat.candidats[i], ""); // Chaîne vide par défaut
-//   }
-//   resultat.nb_candidats = nb_candidats;
-//   resultat.vainqueur = NULL; // Initialiser le nom du vainqueur si nécessaire
-//   resultat.nb_votants = 0;   // Initialiser le nombre de votants
-//   resultat.score = 0;        // Initialiser le score
-//   return resultat;
-// }
-//
-// void free_resultat_uninominale(t_resultat_uninominale *resultat) {
-//   for (int i = 0; i < resultat->nb_candidats; ++i) {
-//     free(resultat->candidats[i]);
-//   }
-//   free(resultat->candidats);
-//   free(resultat->vainqueur);
-// }
-//
-// t_resultat_uninominale uninominale_un_tour(const t_mat_int_dyn *mat_votes, int nb_candidats) {
-//   t_resultat_uninominale resultat = init_resultat_uninominale(nb_candidats);
-//
-//   // Calculer le total des votes pour chaque candidat
-//   int *scores = (int *)malloc(nb_candidats * sizeof(int));
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     scores[i] = 0;
-//     for (int j = 0; j < nb_candidats; ++j) {
-//       scores[i] += mat_votes->data[i][j];
-//     }
-//     resultat.nb_votants +=  scores[i]; // Ajouté : mise à jour du nombre de votants
-//   }
-//
-//   // Trouver le candidat avec le score le plus élevé
-//   int max_score = -1;
-//   int vainqueur_index = -1;
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     if (scores[i] > max_score) {
-//       max_score = scores[i];
-//       vainqueur_index = i;
-//     }
-//   }
-//
-//   // Enregistrer le vainqueur dans la structure de résultat
-//   if (vainqueur_index != -1) {
-//     resultat.vainqueur = strdup(resultat.candidats[vainqueur_index]);
-//     resultat.score = (max_score * 100) / resultat.nb_votants; // Ajouté : calcul du score en pourcentage
-//   }
-//
-//   free(scores);
-//   return resultat;
-// }
-//
-// t_resultat_uninominale uninominale_deux_tours(const t_mat_int_dyn *mat_votes,
-//                                               int nb_candidats) {
-//   t_resultat_uninominale resultat = init_resultat_uninominale(nb_candidats);
-//
-//   // Tableau pour stocker les scores après le premier tour
-//   int *scores_premier_tour = (int *)malloc(nb_candidats * sizeof(int));
-//
-//   // Premier tour
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     scores_premier_tour[i] = 0;
-//     for (int j = 0; j < nb_candidats; ++j) {
-//       scores_premier_tour[i] += mat_votes->data[i][j];
-//     }
-//     resultat.nb_votants +=
-//         scores_premier_tour[i]; // Ajouté : mise à jour du nombre de votants
-//   }
-//
-//   int max_score1 = -1;
-//   int max_score2 = -1;
-//   int index_candidat1 = -1;
-//   int index_candidat2 = -1;
-//
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     if (scores_premier_tour[i] > max_score1) {
-//       max_score2 = max_score1;
-//       index_candidat2 = index_candidat1;
-//
-//       max_score1 = scores_premier_tour[i];
-//       index_candidat1 = i;
-//     } else if (scores_premier_tour[i] > max_score2) {
-//       max_score2 = scores_premier_tour[i];
-//       index_candidat2 = i;
-//     }
-//   }
-//
-//   // Second tour entre les deux candidats restants
-//   int score_candidat1 = 0;
-//   int score_candidat2 = 0;
-//
-//   for (int i = 0; i < nb_candidats; ++i) {
-//     if (i != index_candidat1 && i != index_candidat2) {
-//       // Ajouter les votes pour le second tour
-//       score_candidat1 += mat_votes->data[i][index_candidat1];
-//       score_candidat2 += mat_votes->data[i][index_candidat2];
-//     }
-//   }
-//
-//   if (score_candidat1 > score_candidat2) {
-//     resultat.vainqueur = strdup(resultat.candidats[index_candidat1]);
-//   } else {
-//     resultat.vainqueur = strdup(resultat.candidats[index_candidat2]);
-//   }
-//
-//   resultat.score =
-//       ((score_candidat1 + score_candidat2) * 100) /
-//       resultat.nb_votants; // Ajouté : calcul du score en pourcentage
-//
-//   free(scores_premier_tour);
-//   return resultat;
-// }
-//
-// void afficher_resultat_uninominale(int tour, t_resultat_uninominale resultat) {
-//   printf("Mode de scrutin : uninominal à %d tour(s), %d candidat(s), %d "
-//          "votant(s), vainqueur = %s, score = %d%%\n",
-//          tour, resultat.nb_candidats, resultat.nb_votants, resultat.vainqueur,
-//          resultat.score);
-// }
-//
-// int methodeUninominales() {
-//     const char *nom_fichier = "fich_tests/vote10.csv";
-//     t_vote_format2_data data = lire_fichier_csv_format2(nom_fichier);
-//
-//     // Afficher les informations de la structure
-//     afficher_vote_format2_data(data);
-//   //   char *nom_fichier = "fich_tests/calcul1.csv";
-//   //   t_mat_int_dyn mat_votes = lire_fichier_csv(nom_fichier);
-//   //
-//   //   afficherMatrice(&mat_votes);
-//   //
-//   //
-//   // printf("\n\n\033[1;34m\033[1;33m============================================================\n");
-//   // printf("============================================================\n");
-//   // printf("======================\033[1;31mUNINOMINALE 1 TOUR\033[1;33m====================\n");
-//   // printf("============================================================\n");
-//   // printf("============================================================\n\n\033[0m");
-//   //
-//   // t_resultat_uninominale resultat_un_tour = uninominale_un_tour(&mat_votes, 3);
-//   // afficher_resultat_uninominale(1, resultat_un_tour);
-//   // free_resultat_uninominale(&resultat_un_tour);
-//   //
-//   // printf("\n\n\033[1;34m\033[1;33m============================================================\n");
-//   // printf("============================================================\n");
-//   // printf("======================\033[1;31mUNINOMINALE 2 TOUR\033[1;33m====================\n");
-//   // printf("============================================================\n");
-//   // printf("============================================================\n\n\033[0m");
-//   //
-//   // t_resultat_uninominale resultat_deux_tours =
-//   //     uninominale_deux_tours(&mat_votes, 3);
-//   // afficher_resultat_uninominale(2, resultat_deux_tours);
-//   // free_resultat_uninominale(&resultat_deux_tours);
-//   //
-//   // // Libération de la mémoire
-//   // free_mat_int_dyn(&mat_votes);
-//
-//   return 0;
-// }
+
+
+// fonction principale pour la méthode uninominale
+t_resultat_uninominale methode_uninomale(t_mat_char_star_dyn *votes, bool deuxTours, FILE *fichier) {
+	t_resultat_uninominale resultat;
+	(void)fichier;
+	
+	if (deuxTours) {
+		resultat = uninominale_deux_tours(votes);
+	} else {
+		resultat = uninominale_un_tour(votes);
+	}
+
+	return resultat;
+}
