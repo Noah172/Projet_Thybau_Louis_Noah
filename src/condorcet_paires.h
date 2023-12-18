9d832b5da86a568fe0373994bc206d418291a832
Noah172 <114427265+Noah172@users.noreply.github.com>
2023-12-18 23:42:51 +0100

    Update condorcet_paires.h

--- src/condorcet_paires.h
+++ src/condorcet_paires.h
@@ -1,24 +1,10 @@
 #ifndef CONDORCETPAIRS_H
 #define CONDORCETPAIRS_H
 
-#include "graph.h"
-
-typedef struct {
-    int candidat1;
-    int candidat2;
-    int votesCandidat1;
-    int votesCandidat2;
-} Paire;
-
-typedef struct {
-    Paire *paires;
-    int nbPaires;
-} ResultatPaires;
-
-ResultatPaires initialiserResultatPaires(int nbCandidats);
-
-void ajouterPaire(ResultatPaires *resultat, int candidat1, int candidat2, int votesCandidat1, int votesCandidat2);
-
-char *condorcetPaires(ResultatPaires *resultat, FILE *fichier);
-
+#include "creation_graphe.h"
+void findMaxDuel(Graph *graph, int *max, FILE *fichier);
+void eliminateCandidates(Graph *graph, int *max, FILE *fichier);
+int CondorcetouPaires(t_mat_int_dyn matrice, DynamicList liste_candidat, FILE *fichier, int nb_votants);
+int condorcet_pairs(t_mat_int_dyn matrice, DynamicList liste_candidat, FILE *fichier, int nb_votants);
 #endif
+
