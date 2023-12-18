b57f262ff0a19f6b0432fff6ed65ccbd186ef407
Louis <33205168+OulKra@users.noreply.github.com>
2023-12-18 22:11:33 +0100

    Update creation_graphe.c

--- src/creation_graphe.c
+++ src/creation_graphe.c
@@ -1,89 +1,88 @@
-// #include "types.h"
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
+/// \file creation_graphe.c
+/// \brief Créer le graphe à partir de la matrice de duet et le confirmer
+/// \brief ATTENTION : PAS TERMINÉ
 
+#include "list.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/// \fn compare_arcs(const void *a, const void *b)
+/// \brief Fonction de comparaison pour le tri rapide des arcs du graphe.
+/// \param a Pointeur vers le premier arc.
+/// \param b Pointeur vers le deuxième arc.
+/// \return Valeur négative si le poids du premier arc est inférieur à celui du
+/// deuxième, 0 en cas d'égalité, et positive sinon.
+/*int compare_arcs(const void *a, const void *b) {
+  return ((t_arc *)b)->poids - ((t_arc *)a)->poids;
+}*/
 
-// // Fonction de comparaison pour le tri rapide
-// int compare_arcs(const void *a, const void *b) {
-//   return ((t_arc *)b)->poids - ((t_arc *)a)->poids;
-// }
+/// \fn void trier_graphe(t_graph *graphe)
+/// \brief Fonction pour trier les arcs du graphe dans l'ordre décroissant par
+/// poids. \param graphe Pointeur vers la structure du graphe.
+/*void trier_graphe(t_graph *graphe) {
+  qsort(graphe->arcs, graphe->count, sizeof(t_arc), compare_arcs);
+}*/
 
-// // Fonction pour trier le graphe dans l'ordre décroissant par poids
-// void trier_graphe(t_graph *graphe) {
-//   qsort(graphe->arcs, graphe->count, sizeof(t_arc), compare_arcs);
-// }
+/// \fn void confirmerGraphe(t_graph *graphe)
+/// \brief Fonction pour confirmer le graphe en vérifiant la présence de cycles.
+/// \param graphe Pointeur vers la structure du graphe.
+/*void confirmerGraphe(t_graph *graphe) {
+  for (int i = 0; i < graphe->count; i++) {
+    int *visited = malloc(graphe->count * sizeof(int));
+    int *recStack = malloc(graphe->count * sizeof(int));
+    memset(visited, 0, graphe->count * sizeof(int));
+    memset(recStack, 0, graphe->count * sizeof(int));
 
-// void confirmer_arcs_sans_cycle(t_graph *graphe, int *visited, int *recStack,
-//                                int v) {
-//   visited[v] = 1;
-//   recStack[v] = 1;
+    /// \brief A FINIR
+  }
+}*/
 
-//   for (int i = 0; i < graphe->count; i++) {
-//     if (graphe->arcs[i].confirme) {
-//       if (!visited[i]) {
-//         confirmer_arcs_sans_cycle(graphe, visited, recStack, i);
-//       } else if (recStack[i]) {
-//         // Si un arc crée un cycle, le marquer comme non confirmé
-//         graphe->arcs[v].confirme = 0;
-//       }
-//     }
-//   }
+// Fonction pour créer un nouveau nœud
+Sommet* createNode(char* data) {
+  Sommet* newSommet= (Sommet*)malloc(sizeof(Sommet));
+    newSommet->data = data;
+    newSommet->next = NULL;
+    return newSommet;
+}
 
-//   recStack[v] = 0;
-// }
+// Fonction pour créer un graphe vide
+Graph* createGraph() {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->numNodes = 0;
+    graph->sommets = NULL;
+    return graph;
+}
 
-// void confirmerGraphe(t_graph *graphe) {
-//   for (int i = 0; i < graphe->count; i++) {
-//     // Vérifier si l'ajout de cet arc crée un cycle
-//     int *visited = malloc(graphe->count * sizeof(int));
-//     int *recStack = malloc(graphe->count * sizeof(int));
-//     memset(visited, 0, graphe->count * sizeof(int));
-//     memset(recStack, 0, graphe->count * sizeof(int));
+// Fonction pour ajouter un nœud au graphe
+void addNode(Graph* graph, char* data) {
+  Sommet* newSommet= createNode(data);
 
-//     confirmer_arcs_sans_cycle(graphe, visited, recStack, i);
+    graph->sommets = (Sommet**)realloc(graph->sommets, (graph->numNodes + 1) * sizeof(Sommet*));
 
-//     if (graphe->arcs[i].confirme == 0) {
-//       printf("L'arc %s %s n'est pas confirmé\n", graphe->arcs[i].source,
-//              graphe->arcs[i].destination);
-//     }
-//   }
-// }
+    graph->sommets[graph->numNodes] = newSommet;
+    graph->numNodes++;
+}
 
-// t_graph creer_graphe(t_mat_int_dyn matrice, char **liste_candidat) {
-//   t_graph graphe;
-//   graphe.arcs = malloc((matrice.rows * matrice.rows) * sizeof(t_arc));
-//   graphe.count = 0;
+// Fonction pour ajouter une arête dirigée entre deux nœuds
+void addEdge(Graph* graph, Sommet* src, Sommet* dest) {
+  
+        newSommet->next = graph->sommets[src];
+        graph->sommets[src] = newSommet;
+}
 
-//   // Parcours de la matrice des duels pour créer les arcs du graphe
-//   for (int i = 0; i < matrice.rows; ++i) {
-//     for (int j = 0; j < matrice.rows; ++j) {
+// Fonction pour afficher le graphe
+void printGraph(Graph* graph) {
+    for (int i = 0; i < graph->numNodes; ++i) {
+      Sommet* currentSommet= graph->sommets[i];
+        printf("Nœud %d :", i);
 
-//       int destination = matrice.data[j][i];
-//       int source = matrice.data[i][j];
+        while (currentSommet!= NULL) {
+            printf(" -> %d", currentSommet->data);
+            currentSommet= currentSommet->next;
+        }
 
-//       if (source != 0 && destination != 0 && source != destination &&
-//           source - destination > 0/) {
-//         t_arc arc;
-//         arc.source = liste_candidat[i];
-//         arc.destination = liste_candidat[j];
-//         arc.poids = source - destination;
-
-//         graphe.arcs[graphe.count] = arc;
-//         graphe.count++;
-//       }
-//     }
-//   }
-
-//   trier_graphe(&graphe);
-
-//   return graphe;
-// }
-
-// void afficherGraphe(t_graph graphe) {
-//   for (int i = 0; i < graphe.count; i++) {
-//     printf("Arc (%s, %s, %d)\n", graphe.arcs[i].source,
-//            graphe.arcs[i].destination, graphe.arcs[i].poids);
-//   }
-// }
+        printf("\n");
+    }
+}
